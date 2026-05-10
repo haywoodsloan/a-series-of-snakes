b8aa8fd51399ff65973a4495f71269990f28178f
Sloan Haywood <haywood.sloan@gmail.com>
2026-05-10 15:21:30 -0400

    update deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-05-10',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts'],
-  app: { baseURL: '/a-series-of-snakes/' },
+  app: { baseURL: '/' },
 });
